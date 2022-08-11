bd95e21e8e400fa34d5bee88df9ecf1e9efefa57
Kovacs Tamas <kovacstamas990@gmail.com>
2022-08-11 20:30:26 +0200

    Classes to represent dice rolls are created

---
+++ src/main/java/com/example/calaverabackend/controller/DTO/Dice.java
@@ -0,0 +1,14 @@
+package com.example.calaverabackend.controller.DTO;
+
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class Dice {
+    private String diceNumber;
+    private Integer diceValue;
+
+
+}
--- src/main/java/com/example/calaverabackend/controller/DTO/DiceRoll.java
+++ src/main/java/com/example/calaverabackend/controller/DTO/DiceRoll.java
@@ -4,9 +4,11 @@ package com.example.calaverabackend.controller.DTO;
 import com.example.calaverabackend.model.Player;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @Data
 public class DiceRoll {
-    private HashMap <String, String> diceRolls;
+    private ArrayList<Dice> diceRolls;
+
 }
